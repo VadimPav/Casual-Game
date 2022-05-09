99b1a4012e83c49ca57b676040145ed863c4e583
VadimPav <v.pavlov0607@gmail.com>
2022-05-09 23:29:47 +0300

    Game Done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,52 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private KeyCode keyOne;
     [SerializeField] private KeyCode keyTwo;
+    [SerializeField] private int z;
+    [SerializeField] private int x;
+    [SerializeField] private Rigidbody rb;
     [SerializeField] private Vector3 moveDirection;
-    private Rigidbody _rb;
 
     private void Start()
     {
-        _rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+    }
+    
+#if UNITY_ANDROID || UNITY_EDITOR
+    public void MoveVertical(int inputAxis)
+    {  
+        z = inputAxis;
+        rb.velocity = new Vector3(0, 0, rb.velocity.z + z);
     }
 
     private void FixedUpdate()
     {
-        if (Input.GetKey(keyOne))
-            _rb.velocity += moveDirection;
-        if (Input.GetKey(keyTwo))
-            _rb.velocity -= moveDirection;
+        rb.velocity = new Vector3 (x , moveDirection.y, z);
     }
-
+    public void MoveHorizontal(int inputAxis)
+    {   
+        x = inputAxis;
+        rb.velocity = new Vector3(x, 0, 0);
+    }
+#elif UNITY_EDITOR || UNITY_64    
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(keyOne))
+            rb.velocity  += moveDirection;
+        if (Input.GetKey(keyTwo))
+            rb.velocity -= moveDirection;
+    }
+#endif   
     
 }
